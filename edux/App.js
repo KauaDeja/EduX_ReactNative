80099af4d6e0bbc90d840d7550efd2b96c8b8fcb
Gabriel <gabriel.gaspargbg@gmail.com>
2020-11-26 08:57:38 -0300

    problemas em background objetivo

--- edux/App.js
+++ edux/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MenuEdux from './components/menu';
+import Objetivo from './pages/Objetivos'
 
 
 export default function App() {
   return (
-  <MenuEdux />    
+  <Objetivo />    
   );
 }
 
--- edux/pages/Objetivos/index.js
+++ edux/pages/Objetivos/index.js
@@ -1,2 +1,31 @@
 import React from 'react';
-import MenuEdux from './components/menu';
+import MenuEdux from '../../components/menu';
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import background from '../../assets/img/educacao1.jpg'
+
+const image = {background};
+
+const Objetivo = () =>{
+    return(
+     <MenuEdux/>   
+    <View style={styles.container}>
+    <ImageBackground source={image} style={styles.image}>
+    </ImageBackground>
+    </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "column"
+    },
+    image: {
+      flex: 1,
+      resizeMode: "cover",
+      justifyContent: "center"
+    }
+  })
+
+export default Objetivo
+
+
