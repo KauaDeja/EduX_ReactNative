9492a0398e7181b85b412b5316b5e5e94ce72d27
Gabriel-maia-santos <gabis.maia.santos@gmail.com>
2020-12-04 11:32:53 -0300

    AlunoTurma finalizado

---
+++ edux/components/AlunoTurma.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Linking } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#F7F7F7',
+      marginTop:60
+    },
+    listItem:{
+      margin:10,
+      padding:10,
+      backgroundColor:"#FFF",
+      width:"80%",
+      flex:1,
+      alignSelf:"center",
+      flexDirection:"row",
+      borderRadius:5
+    }
+  });
+
+const AlunoTurmas = (Aluno) => {
+    const {matricula, IdUsuario, IdTurma} = Aluno;
+
+    return (
+        <View style={styles.listItem}>
+            <View style={{alignItems:"center",flex:1}}>
+                <Text style={{fontWeight:"bold"}}>{matricula}</Text>
+                <Text style={{fontWeight:"bold"}}>{IdUsuario}</Text>
+                <Text style={{fontWeight:"bold"}}>{IdTurma}</Text>
+            </View>
+            
+        </View>
+    )
+}
+
+export default AlunoTurmas;
---
+++ edux/pages/AlunoTurma/index.js
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import AlunoTurmas from '../../components/AlunoTurma';
+
+import AlunoTurma from './components/AlunoTurmas'
+
+
+const AlunoTurma = () => {
+
+    const [matricula, setMatricula] = useState('');
+    const [idUsuario, setUsuario] = useState([]);
+    const [idTurma, setTurma] = useState([]);
+    
+    useEffect(() => {
+      Get();
+  }, []); 
+
+    const Get = () => {
+      fetch('http://192.168.0.21:5000/api/AlunoTurma')
+          .then(response => response.json())
+          .then(data => {
+              setMatricula(data.data)
+              setTurma(data.data)
+              setUsuario(data.data)
+              console.log(data.data);
+          })
+          .catch(err => console.error(err));
+  }
+
+
+  const alunoTurma = ({ item }) => (
+    <AlunoTurmas matricula={item.matricula} idusuario={item.idUsuario} idturma={item.idTurma} />
+  );
+
+    return(
+        <View style={styles.container}>
+          <Text style ={styles.Matricula}>Matricula : </Text>  
+            <View style={styles.flat}>
+          <FlatList
+                data={matricula}
+                renderItem={alunoTurma}
+                
+                />
+          </View>
+          </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    
+    Matricula: {
+      fontSize: 20,
+      fontFamily: 'sans-serif',
+      padding: 15,
+      color: 'purple',
+    },
+    
+    flat: {
+      width: 400,
+      borderColor: 'purple',
+      
+    }
+
+  });
+
+  export default AlunoTurma;
